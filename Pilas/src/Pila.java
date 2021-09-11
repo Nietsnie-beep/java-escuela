61b3938b4d1661800ecf8d59c3f4a7a315081e89
unknown <AL02982380@tecmilenio.mx>
2021-09-10 23:16:32 -0500

    first commit

---
+++ Pilas/src/Pila.java
@@ -0,0 +1,38 @@
+public class Pila {
+    
+    String stack[];
+    int maximo;
+    int ultimo;
+
+    //constructor
+    public Pila(int n){
+        maximo = n;
+        stack = new String[maximo];
+        ultimo = 0;
+    }
+
+    public void push(String str){
+        if(ultimo < maximo){
+            stack[ultimo] = str;
+            ultimo++;
+        }
+    }
+    
+    public Boolean empty(){
+        if(ultimo == 0){
+            return true;
+        }else{
+            return false;
+        }
+    } 
+
+    public String pop(){
+        String temporal = null;
+        if(ultimo > 0){
+            temporal = stack[ultimo -1];
+            stack[ultimo-1] = null;
+            ultimo--;
+        }
+        return temporal;
+    }
+}
